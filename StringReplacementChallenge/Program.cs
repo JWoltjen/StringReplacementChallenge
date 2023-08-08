eb67ce42de31ca29be98ca9c59901667060d7284
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2023-08-08 11:23:03 -0500

    Complete bonus challenge

--- StringReplacementChallenge/Program.cs
+++ StringReplacementChallenge/Program.cs
@@ -1,10 +1,19 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace StringReplacementChallenge
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            // PrimaryChallenge();
+            BonusChallenge();
+
+            Console.ReadLine();
+        }
+
+        private static void PrimaryChallenge()
         {
             List<string> primaryLines = File.ReadAllLines(@"C:\Users\Jeff's PC\source\repos\StringReplacementChallenge\StringReplacementChallenge\primary.txt").ToList();
 
@@ -41,5 +50,33 @@ namespace StringReplacementChallenge
 
             Console.ReadLine();
         }
+
+        private static void BonusChallenge()
+        {
+            string bonusText = File.ReadAllText(@"C:\Users\Jeff's PC\source\repos\StringReplacementChallenge\StringReplacementChallenge\bonus.txt");
+
+            
+
+            // {FirstName}
+            var results = Regex.Matches(bonusText, @"{[^{}]+}")
+                // .Cast<Match>() converts the collection of matches to an enumerable sequence of Match objects.
+                // Match object represents the result of a single successful pattern match within a string.
+                .Cast<Match>()
+                // takes each match and extracts the value inside the curly braces by using the Substring method. It removes the opening and closing curly braces, leaving just the text inside (e.g., FirstName from {FirstName}).
+                .Select(x => x.Value.Substring(1, x.Value.Length -2))
+                // converts the resulting sequence of strings into a HashSet<string>, which is a collection that contains no duplicate elements.
+                .ToHashSet();
+
+            foreach(var result in results)
+            {
+                Console.Write($"Please give me the value for {result}: ");
+                bonusText = bonusText.Replace("{" + result + "}", Console.ReadLine());
+            }
+
+            File.WriteAllText(@"C:\Users\Jeff's PC\source\repos\StringReplacementChallenge\StringReplacementChallenge\bonus.txt", bonusText);
+
+            Console.WriteLine("Text file updated");
+        }
     }
 }
+
